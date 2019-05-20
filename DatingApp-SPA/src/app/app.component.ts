9abb5357c096a085a508ad8236501a97b36fade6
dane.duplessis <dane.duplessis@entelect.co.za>
2019-05-20 11:04:07 +0200

    end of secton 7

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
