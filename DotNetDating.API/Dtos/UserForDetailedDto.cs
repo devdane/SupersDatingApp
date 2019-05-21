a2b8a9b613ef56096774208836986d89539a5e71
dane.duplessis <dane.duplessis@entelect.co.za>
2019-05-21 07:45:50 +0200

    End of Module 8 - extending the api

--- DotNetDating.API/Dtos/UserForDetailedDto.cs
+++ DotNetDating.API/Dtos/UserForDetailedDto.cs
@@ -10,6 +10,7 @@ namespace DotNetDating.API.Dtos
         public string Username { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
@@ -19,6 +20,6 @@ namespace DotNetDating.API.Dtos
         public string City { get; set; }
         public string Country { get; set; }
         public string PhotoUrl { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
