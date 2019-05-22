73611f6a7fc9ee1f6988c8ca09d5171557d14d7c
dane.duplessis <dane.duplessis@entelect.co.za>
2019-05-22 17:08:52 +0200

    starting 110

--- DotNetDating.API/Dtos/PhotoForReturnDto.cs
+++ DotNetDating.API/Dtos/PhotoForReturnDto.cs
@@ -1,8 +1,8 @@
 using System;
 
-namespace DotNetDating.API.Models
+namespace DotNetDating.API.Dtos
 {
-    public class Photo
+    public class PhotoForReturnDto
     {
         public int Id { get; set; }
         public string Url { get; set; }
@@ -10,7 +10,5 @@ namespace DotNetDating.API.Models
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
-        public User User { get; set; }
-        public int UserId { get; set; }
     }
 }
--- DotNetDating.API/Helpers/AutoMapperProfiles.cs
+++ DotNetDating.API/Helpers/AutoMapperProfiles.cs
@@ -25,6 +25,8 @@ namespace DotNetDating.API.Helpers
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
